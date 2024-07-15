e1b8581c0b63d3a6a74a71a4f928cbcd09f92dc1
Chris Hayen <chayen@gmail.com>
2024-07-15 19:54:55 -0400

    Did some stuff

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,16 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- " Show hover
+map("n", "K", vim.lsp.buf.hover, {desc="Show hover"})
+-- nnoremap K <Cmd>lua vim.lsp.buf.hover()<CR>
+ -- " Jump to definition
+-- nnoremap gd <Cmd>lua vim.lsp.buf.definition()<CR>
+ -- " Open code actions using the default lsp UI, if you want to change this please see the plugins above
+-- nnoremap <leader>ca <Cmd>lua vim.lsp.buf.code_action()<CR>
+ -- " Open code actions for the selected visual range
+-- xnoremap <leader>ca <Cmd>lua vim.lsp.buf.range_code_action()<CR>
+map("n", "ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>")
+-- map("n", "<leader>ca", "<cmd>vim.lsp.buf.range_code_action()<cr>")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -33,6 +33,15 @@ return {
       { ",v", "<cmd>VenvSelect<cr>" },
     },
   },
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
   --
   -- {
   -- 	"williamboman/mason.nvim",
